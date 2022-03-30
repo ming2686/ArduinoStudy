4d0017dfbcb3e7631c22bf18cc1dd16d129ed0fe
ming <ming2686@naver.com>
2022-03-30 15:00:28 +0900

    0330study

--- arduino_src.ino
+++ arduino_src.ino
@@ -1,10 +1,30 @@
-void setup(){
+int led = 13 ; // LED on arduino
+int digitalPin = 3; // linear Hall magnetic sensor digital interface
+int analogPin = A0; // linear Hall magnetic sensor analog interface
+int digitalVal ; // digital readings
+int analogVal; // analog readings
+void setup ()
+{
+  pinMode (led, OUTPUT); 
+  pinMode (digitalPin, INPUT); 
+  //pinMode(analogPin, INPUT); 
   Serial.begin(9600);
 }
-
-void loop(){
-  for(int i=0;i<100;i++){
-    Serial.println(i);
-    delay(500);
+void loop ()
+{
+  // Read the digital interface
+  digitalVal = digitalRead(digitalPin) ; 
+  if (digitalVal == HIGH) // When magnetic field is present, Arduino LED is on
+  {
+    digitalWrite (led, HIGH);
   }
+  else
+  {
+    digitalWrite (led, LOW);
+  }
+  
+  // Read the analog interface
+  analogVal = analogRead(analogPin);
+  Serial.println(analogVal); // print analog value
+  delay(100);
 }
